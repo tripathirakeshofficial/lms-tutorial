b16b4c06845837f905f3a94fea9b827979ead1f3
Rakesh Tripathi <tripathirakeshofficial@gmail.com>
2023-09-26 18:09:10 +0530

    Dynamic Layout

---
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,5 @@
+const AnalyticsPage = () => {
+  return <div>Analytics Page</div>;
+};
+
+export default AnalyticsPage;
--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Layout, Compass } from "lucide-react";
+import { Layout, Compass, List, BarChart } from "lucide-react";
+import { usePathname } from "next/navigation";
+
 import { SidebarItem } from "./sidebar-item";
 
 const guestRoutes = [
@@ -16,8 +18,25 @@ const guestRoutes = [
   },
 ];
 
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
+
 export const SidebarRoutes = () => {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.includes("/teacher");
+
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
   return (
     <div className="flex flex-col w-full">
