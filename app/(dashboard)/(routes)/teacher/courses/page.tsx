caafaa16a13e656651062ca47dfb67561aa6c099
Rakesh Tripathi <tripathirakeshofficial@gmail.com>
2023-09-26 19:03:07 +0530

    Course Creation UI

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 const CoursesPage = () => {
-  return <div>Courses Page</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default CoursesPage;
