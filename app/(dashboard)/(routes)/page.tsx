8e3c2edace6550e8f74bd6aa493b81a2178f7719
Rakesh Tripathi <tripathirakeshofficial@gmail.com>
2023-09-26 15:32:21 +0530

    Authentication

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <p>This is a protected page</p>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
